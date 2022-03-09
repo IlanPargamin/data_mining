cd27b9988c6a651f15496239bc4284589d9fa9ed
Ilan Pargamin <ilan.pargamin@gmail.com>
2022-03-09 13:02:33 +0200

    added the argparse functionalities + prints results in csv file. This is the first commit of the second checkpoint.

--- web_scraping.py
+++ web_scraping.py
@@ -6,6 +6,27 @@ To choose the range of the pages to scrape, you can modify the global variables 
 from get_main import get_main
 from get_urls import get_urls
 from get_project import get_project
+import logging
+import argparse
+import csv
+
+
+# def setup_logger(name, log_file, level=logging.INFO):
+#     """
+#     To setup as many loggers as needed
+#     """
+#
+#     handler = logging.FileHandler(log_file)
+#     handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+#
+#     a_logger = logging.getLogger(name)
+#     a_logger.setLevel(level)
+#     a_logger.addHandler(handler)
+#
+#     return a_logger
+#
+#
+# stdout = setup_logger('stdout', 'stdout.log')
 
 
 def join_lists_of_dicts(dict_list1, dict_list2):
@@ -23,16 +44,77 @@ def join_lists_of_dicts(dict_list1, dict_list2):
     return dict_list1
 
 
-def web_scraping():
+def export_to_csv(list_of_dicts):
+    """
+    export a list of dictionaries to a csv file
+    """
+    keys = list_of_dicts[0].keys()
+    with open('output.csv', 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(list_of_dicts)
+
+
+def web_scraping(run_get_project=True):
     """
     This function is the main function. It returns a list of dictionaries where each dictionary contains data on a
     specific job offer from freelancer.com.
+    If run_get_project=True, we run the function get_project and collect more detailed data on each project.
     """
     dicts_main = get_main()
-    dicts_projects = get_project(get_urls(dicts_main))
-    return join_lists_of_dicts(dicts_main, dicts_projects)
+    if run_get_project:
+        dicts_projects = get_project(get_urls(dicts_main))
+        merged_dic = join_lists_of_dicts(dicts_main, dicts_projects)
+        # stdout.info(merged_dic)
+
+        export_to_csv(merged_dic)
+
+        return merged_dic
+    else:
+        # stdout.info(dicts_main)
+        export_to_csv(dicts_main)
+
+        return dicts_main
+
+
+def page2page(page_start, page_stop):
+    """
+    modify our globals by print the terminal arguments in the file globals.py
+    """
+    file_path = 'globals.py'
+    with open(file_path, 'w') as f:
+        f.write(f"""MAIN_URL = 'https://www.freelancer.com'\nPAGE_START = {page_start}\nPAGE_STOP = {page_stop}""")
+
+
+def my_parser():
+    """
+    parses the arguments from the command line
+    User must input the pages range she wants to scrape (page_start and page_stop).
+    If the user does not want details on the projects (i.e. collect what the function get_project returns) then
+    she must add -not in the console command.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('page_start')
+    parser.add_argument('page_stop')
+    parser.add_argument('-not', '--no_scrape_all', action='store_true')
+    args = parser.parse_args()
+
+    # if not isinstance(int(args.page_start), int) or not isinstance(int(args.page_stop), int):
+    #     print(f'input must be digit')
+    #     sys.exit(1)
+
+    if args.no_scrape_all:
+        page2page(args.page_start, args.page_stop)
+        print(f"scraping freelancer.com/jobs from page {args.page_start} to page {args.page_stop} "
+              f"main page only")
+        web_scraping(run_get_project=False)
+
+    else:
+        page2page(args.page_start, args.page_stop)
+        print(f"scraping freelancer.com/jobs from page {args.page_start} to page {args.page_stop} "
+              f"including project pages")
+        web_scraping(run_get_project=True)
 
 
 if __name__ == "__main__":
-    merged_dicts = web_scraping()
-    print()
+    my_parser()
